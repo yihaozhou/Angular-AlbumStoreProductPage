1a5a0d275d2aeb68c6ef79035d5fcb9deb3c81ca
Max Zhou <Yihao@MacBook-Pro.local>
2018-01-23 23:29:47 -0500

    WIP Part 3

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(response => response.json());
+  }
+
+}
